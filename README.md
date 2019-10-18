10e1fe80df86ec9dff4ab6865193405e1121f92a
mwyman <mwyman@anl.gov>
2019-10-18 09:42:06 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Genetric Algorith test
+
+## Notebooks
+- GATestDNA.ipynb
+- GATestMotors2.ipynb
+- GATestMotors.ipynb
+- GATestNums.ipynb
+- Traveling Salesman Problem.ipynb
+
+## Scripts
+-TSP.py
+
+## Results
+- gaMotors_OM-False.png
+- gaMotors_OM-True.png
+- testPlot2.png
+- testPlot.png
---
+++ TSP.py
@@ -0,0 +1,46 @@
+class city:
+	def __init__(self, label, xpos, ypos):
+			self.name = label
+			self.x = xpos
+			self.y = ypos
+			
+class route:
+	def __init__(self, cityList)
+		self.route = cityList
+		self.distance = 0.0
+		self.fitness = 0.0
+	
+	def distance(self):
+		self.distance = 0
+		for i in range(len(self.route)+1):
+			
+	
+class ga:
+	def __init__(self, routes)
+		self.pop = routes
+		
+	def rank(self):
+		rankings =
+		return np.argsort
+
+	def matingPool:
+		
+	def mate:
+		
+	def mutate:
+		
+	def mutatePop:
+
+	def go(self, nGenerations, nPopulation, nElite = 2, Pc = 0.8, Pm = 0.05, 
+			verbose = False, plot = False)
+
+	def plotRoute:
+
+	def plotPop:
+
+def bruteForce(cityList):
+	route = 
+	
+
+	
+
